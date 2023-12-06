9cf809b43ccfdc75d7a225f38a1b8b555d2779f7
Noa Spele <noa@spele.se>
2023-12-06 10:53:07 +0100

    Added solution to day 5 and cleaned code

--- 2023/dec06.py
+++ 2023/dec06.py
@@ -1,19 +1,12 @@
-import re
 import sys
-import math
-from copy import deepcopy
-from collections import deque
 
 ans1, ans2 = 1, 0
 lines = open(sys.argv[1]).read().split('\n')
-times = lines[0].split()
-times = times[1:]
-dists = lines[1].split()
-dists = dists[1:]
+times, dists = lines[0].split(), lines[1].split()
+times, dists = times[1:], dists[1:]
 
 for i in range(len(times)):
-    t = int(times[i])
-    hs = int(dists[i])
+    t, hs = int(times[i]), int(dists[i])
     curr = 0
     for j in range(1,t):
         cs = (t-j)*j
@@ -23,21 +16,19 @@ for i in range(len(times)):
 
 print(f'Answer Part 1: {ans1}')
 
-time = int(''.join(times))
-dist = int(''.join(dists))
+time, dist = int(''.join(times)), int(''.join(dists))
 mint = 0
 for t in range(1,time):
     if (time-t)*t > dist:
         mint = t
         break
 
-maxt = 0
-t = time-1
+maxt, t = 0, time-1
 while t > 1:
     if (time-t)*t > dist:
         maxt = t
         break
     t -= 1
-    
+
 ans2 = maxt - mint + 1
 print(f'Answer Part 2: {ans2}')
