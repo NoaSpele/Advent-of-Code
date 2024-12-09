29d50314634d0e173811be442f3a335d63e9da9c
Noa Spele <noa@spele.se>
2024-12-09 12:14:13 +0100

    Added improved solution to day 6 2024

--- 2024/dec06.kt
+++ 2024/dec06.kt
@@ -1,53 +1,44 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
-data class Pos (val x: Int, val y: Int)
 data class DirPos (val x: Int, val y: Int, val dx: Int, val dy: Int)
 
 fun main(args: Array<String>) {
-    val map: List<List<Char>> = Path(args[0]).readLines().map { it.toList() }
+    val map: List<MutableList<Char>> = Path(args[0]).readLines().map { it.toMutableList() }
     val sy = map.indexOf(map.find { row -> row.contains('^') })
     val sx = map[sy].indexOf('^')
 
     val ans1Set = checkPath(sx, sy, map)
     println("Ans 1: ${ans1Set.size}")
 
-    var ans2 = 0; var numChecked = 0
-    ans1Set.forEach { p ->
-        val mapCopy = map.map { it.toMutableList() }
-        mapCopy[p.y][p.x] = '#'
-        if (checkPath(sx, sy, mapCopy).size == 0) ans2 += 1
-        numChecked += 1
-        if (numChecked % 1000 == 0) println("Num checked: $numChecked")
+    val ans2 = ans1Set.sumOf { p ->
+        map[p.second][p.first] = '#'
+        val numPos = checkPath(sx, sy, map).size
+        map[p.second][p.first] = '.'
+        if (numPos == 0) 1 else 0L
     }
     println("Ans 2: $ans2")
 }
 
-fun checkPath(sx: Int, sy: Int, map: List<List<Char>>): MutableSet<Pos> {
-    val visited: MutableSet<Pos> = mutableSetOf()
+fun checkPath(sx: Int, sy: Int, map: List<List<Char>>): MutableSet<Pair<Int, Int>> {
+    val visited: MutableSet<Pair<Int, Int>> = mutableSetOf()
     val loopSet: MutableSet<DirPos> = mutableSetOf()
     val w = map[0].size; val h = map.size
-    var x = sx; var y = sy; var dir = arrayOf(0, -1)
+    var x = sx; var y = sy; var dirIdx = 3
 
     while (true) {
-        if (Pos(x,y) !in visited) visited += Pos(x,y)
-        if (DirPos(x,y,dir[0],dir[1]) in loopSet) return mutableSetOf()
-        else loopSet += DirPos(x,y,dir[0],dir[1])
-        val dx = dir[0]; val dy = dir[1]
-        var tx = x + dx; var ty = y + dy
-        if (tx < 0 || tx > w-1 || ty < 0 || ty > h-1) break
-        while (map[ty][tx] == '#') {
-            dir = changeDirection(dir[0], dir[1])
-            tx = x + dir[0]; ty = y + dir[1]
-        }
-        x = tx; y = ty
+        val (dx, dy) = getDirection(dirIdx)
+        if (Pair(x,y) !in visited) visited += Pair(x, y)
+        if (DirPos(x, y, dx, dy) in loopSet) return mutableSetOf()
+        else loopSet += DirPos(x, y, dx, dy)
+        val tx = x + dx; val ty = y + dy
+        if (tx !in 0..<w || ty !in 0..<h) break
+        if (map[ty][tx] == '#') dirIdx = (dirIdx + 1) % 4
+        else { x = tx; y = ty }
     }
     return visited
 }
 
-fun changeDirection(dx: Int, dy: Int): Array<Int> {
-    if (dx == 0 && dy == -1) return arrayOf(1,0)
-    else if (dx == 1 && dy == 0) return arrayOf(0,1)
-    else if (dx == 0 && dy == 1) return arrayOf(-1,0)
-    else return arrayOf(0,-1)
-}
+data class Dir(val dx: Int, val dy: Int)
+val dirList = listOf(Dir(1,0), Dir(0,1), Dir(-1,0), Dir(0,-1))
+fun getDirection(dirIdx: Int): Dir { return dirList[dirIdx] }
