c69f749249a42e08b30bbe6858962f44dc40765a
Noa Spele <noa@spele.se>
2024-12-09 11:31:54 +0100

    Added improved solution to day 9 2024

--- 2024/dec09.kt
+++ 2024/dec09.kt
@@ -4,52 +4,53 @@ import kotlin.io.path.readLines
 fun main(args: Array<String>) {
     val diskMap: String = Path(args[0]).readLines()[0]
     val fileSystem: MutableList<Long> = mutableListOf()
-    val files: MutableList<Int> = mutableListOf()
+    val files: MutableList<Pair<Int, Int>> = mutableListOf()
+    val spaces: MutableList<Pair<Int, Int>> = mutableListOf()
     var isFile = true; var fileId = 0L
     diskMap.map { it.digitToInt() }.forEach { num ->
         if (isFile) {
-            files += num
+            files += Pair(fileSystem.size, num)
             (0..<num).forEach { _ -> fileSystem += fileId }
             fileId += 1
-        } else (0..<num).forEach { _ -> fileSystem += -1 }
+        } else {
+            spaces += Pair(fileSystem.size, num)
+            (0..<num).forEach { _ -> fileSystem += -1 }
+        }
         isFile = !isFile
     }
-
-    val orgFileSys = fileSystem.toMutableList()
-    println("Ans 1: ${solveP1(fileSystem)}")
-    println("Ans 2: ${solveP2(files, orgFileSys)}")
+    println("Ans 1: ${solveP1(fileSystem.toMutableList())}")
+    println("Ans 2: ${solveP2(files, spaces, fileSystem)}")
 }
 
 fun solveP1(fileSystem: MutableList<Long>): Long {
-    var numIdx: Int; var emptyIdx: Int
+    var numIdx = fileSystem.size -1; var emptyIdx = 0
     while (true) {
-        emptyIdx = fileSystem.indexOfFirst { it == -1L }
-        numIdx = fileSystem.indexOfLast { it != -1L }
+        emptyIdx = (emptyIdx..<fileSystem.size).first { fileSystem[it] == -1L }
+        numIdx = (0..numIdx).reversed().first { fileSystem[it] != -1L }
         if (emptyIdx > numIdx) break
         fileSystem[emptyIdx] = fileSystem[numIdx]
         fileSystem[numIdx] = -1L
     }
-    return fileSystem.subList(0, emptyIdx+1).indices.sumOf {
-        if (fileSystem[it] == -1L) 0
-        else fileSystem[it] * it
-    }
+    return fileSystem.subList(0, emptyIdx).indices.sumOf { fileSystem[it] * it }
 }
 
-fun solveP2(files: List<Int>, fileSystem: MutableList<Long>): Long {
+fun solveP2(
+    files: List<Pair<Int, Int>>, spaces: MutableList<Pair<Int, Int>>,
+    fileSystem: MutableList<Long>
+): Long {
     files.indices.reversed().map { idx ->
-        val currIdx = fileSystem.indexOfFirst { it == idx.toLong() }
-        val newIdx = (0..<currIdx).find {
-            fileSystem[it] == -1L && fileSystem.subList(it, it + files[idx]).toSet().size == 1
-        }
-        if (newIdx != null && newIdx < currIdx) {
-            (0..<files[idx]).forEach {
-                fileSystem[newIdx + it] = idx.toLong()
-                fileSystem[currIdx + it] = -1
+        val spaceIdx = spaces.indexOfFirst { it.second >= files[idx].second }
+        if (spaceIdx != -1 && spaces[spaceIdx].first < files[idx].first) {
+            val curr = spaces[spaceIdx]
+            (0..<files[idx].second).forEach {
+                fileSystem[curr.first + it] = idx.toLong()
+                fileSystem[files[idx].first + it] = -1
             }
+            spaces[spaceIdx] = Pair(curr.first + files[idx].second, curr.second - files[idx].second)
+            if (spaces[spaceIdx].second == 0) spaces.removeAt(spaceIdx)
         }
     }
     return fileSystem.indices.sumOf {
-        if (fileSystem[it] == -1L) 0
-        else fileSystem[it] * it
+        if (fileSystem[it] == -1L) 0 else fileSystem[it] * it
     }
 }
