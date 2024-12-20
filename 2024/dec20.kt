d1f28a02085fec3ad22d0272324b5b85dab98e87
Noa Spele <noa@spele.se>
2024-12-20 18:46:52 +0100

    Added improved solution to day 20 2024

--- 2024/dec20.kt
+++ 2024/dec20.kt
@@ -1,5 +1,6 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.math.abs
 
 fun main(args: Array<String>) {
     val map: List<MutableList<Char>> = Path(args[0]).readLines().map { it.toMutableList() }
@@ -10,72 +11,51 @@ fun main(args: Array<String>) {
     } }
 
     val (_, path) = findShortest(sPos, ePos, map)
-    val ans1 = findNumCheats(sPos, path, map, false, 2)
+    val ans1 = findNumCheats(sPos, path, map, 2)
     println("Ans 1: $ans1")
-    val ans2 = findNumCheats(sPos, path, map, true, 20)
+    val ans2 = findNumCheats(sPos, path, map, 20)
     println("Ans 2: $ans2")
 }
 
-private val dirs = listOf(Pair(1,0), Pair(-1,0), Pair(0,1), Pair(0,-1))
+private val dirs = listOf(
+    DirInv(1,0,0,1),
+    DirInv(-1,0,1,0),
+    DirInv(0,1,2,3),
+    DirInv(0,-1,3,2))
 private val seenDists: MutableMap<Pair<Pair<Int, Int>, Pair<Int, Int>>, Int> = mutableMapOf()
+data class QO(val x: Int, val y: Int, val d: Int, val dir: Int, val p: List<Pair<Int, Int>> )
 fun findShortest(sPos: Pair<Int, Int>, ePos: Pair<Int, Int>, map: List<List<Char>>): Pair<Int, List<Pair<Int, Int>>> {
-    val queue:MutableList<Triple<Pair<Int, Int>, Int, List<Pair<Int, Int>>>>
-        = mutableListOf(Triple(sPos, 0, listOf()))
+    val queue:MutableList<QO> = mutableListOf(QO(sPos.first, sPos.second, 0, -1, listOf()))
     val seen: MutableSet<Pair<Int, Int>> = mutableSetOf()
     if (Pair(sPos, ePos) in seenDists) return Pair(seenDists[Pair(sPos, ePos)]!!, listOf())
 
     while (queue.isNotEmpty()) {
-        val (pos,d,path) = queue.removeFirst()
-        val (x,y) = pos
-        if (x !in 0..map[0].size || y !in map.indices) continue
+        val (x,y,d,dir,path) = queue.removeFirst()
+        if (x !in 1..<map[0].size-1 || y !in 1..<map.size-1) continue
         if (Pair(x,y) in seen || map[y][x] == '#') continue
         if (x == ePos.first && y == ePos.second) {
             seenDists[Pair(sPos, ePos)] = d
             return Pair(d, path)
         }
-        seenDists[Pair(sPos, pos)] = d
-        seen += Pair(x,y)
-        dirs.forEach { (dx, dy) ->
-            queue.addLast(Triple(Pair(x+dx, y+dy), d+1, path + Pair(x, y)))
+        seenDists[Pair(sPos, Pair(x,y))] = d; seen += Pair(x,y)
+        dirs.forEach { (dx, dy, nd, inv) ->
+            if (dir != inv) queue.addLast(QO(x+dx, y+dy, d+1, nd,path + Pair(x, y)))
         }
     }
     seenDists[Pair(sPos, ePos)] = -1
     return Pair(-1, listOf())
 }
 
-private fun findOther(
-    s: Pair<Int, Int>, m: List<List<Char>>, l: Int
-): List< Pair< Pair<Int, Int>, Int> > {
-    val queue:MutableList<Pair<Pair<Int, Int>, Int>> = mutableListOf(Pair(s, 0))
-    val seen: MutableSet<Pair<Int, Int>> = mutableSetOf()
-    val toRet: MutableList<Pair<Pair<Int, Int>, Int>> = mutableListOf()
-
-    while (queue.isNotEmpty()) {
-        val (pos,d) = queue.removeFirst()
-        val (x,y) = pos
-        if (x !in 1..<m[0].size-1 || y !in 1..<m.size-1) continue
-        if (Pair(x,y) in seen || d>l) continue
-        if (m[y][x] != '#') toRet += Pair(Pair(x,y),d)
-        seen += Pair(x,y)
-        dirs.forEach { (dx, dy) ->
-            queue.addLast(Pair(Pair(x+dx, y+dy), d+1))
-        }
-    }
-    return toRet
-}
-
 private fun findNumCheats(
-    sPos: Pair<Int, Int>, path: List<Pair<Int, Int>>,
-    map: List<List<Char>>, print: Boolean, cLim: Int
+    sPos: Pair<Int, Int>, path: List<Pair<Int, Int>>, map: List<List<Char>>, cLim: Int
 ): Long {
-    return path.indices.sumOf { i ->
-        val p = path[i]
-        if (print && (i+1) % 3000 == 0) println("${i+1}/${path.size}")
-        val (d1,_) = findShortest(sPos, p, map)
-        val other = findOther(p,map,cLim)
-        other.sumOf { (o,d) ->
-            val (d2,_) = findShortest(sPos, o, map)
-            if (d1 + d <= d2-100) 1 else 0L
+    return path.sumOf { p1 ->
+        val (d1,_) = findShortest(sPos, p1, map)
+        path.sumOf { p2 ->
+            val dist = abs(p1.first - p2.first) + abs(p1.second - p2.second)
+            if (p1 != p2 && dist <= cLim) {
+                if (d1 + dist <= findShortest(sPos, p2, map).first - 100) 1 else 0
+            } else 0L
         }
     }
 }
